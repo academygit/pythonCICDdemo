d4cb5287da2a504dff7f069a17acd48ae2c39da6
Samu Kauppinen <samu.kauppinen@academy.fi>
2020-08-24 02:46:13 +0300

    some changes to calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,6 @@
 """
 Calculator library containing basic math operations.
 """
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("first", help="first, argument to be added", type=int)
-parser.add_argument("second", help="second, argument to be added", type=int)
-args = parser.parse_args()
 
 def add(first_term, second_term):
     return first_term + second_term
@@ -16,5 +10,5 @@ def subtract(first_term, second_term):
     return first_term - second_term
 
 if __name__ == "__main__":
-    print(f'{args.first} + {args.second} = {add(args.first, args.second)}')
-    print(f'{args.first} - {args.second} = {subtract(args.first, args.second)}')
+    add(1, 2)
+    subtract(3, 2)
