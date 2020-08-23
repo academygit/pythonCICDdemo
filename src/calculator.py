e4070d0d01ff7be9b8f0036bcc926cf9adaac506
Samu Kauppinen <samu.kauppinen@academy.fi>
2020-08-24 02:23:41 +0300

    Change in the content and deployment location

--- src/calculator.py
+++ src/calculator.py
@@ -1,7 +1,12 @@
 """
 Calculator library containing basic math operations.
 """
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("first", help="first, argument to be added", type=int)
+parser.add_argument("second", help="second, argument to be added", type=int)
+args = parser.parse_args()
 
 def add(first_term, second_term):
     return first_term + second_term
@@ -9,3 +14,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+if __name__ == "__main__":
+    print(f'{args.first} + {args.second} = {add(args.first, args.second)}')
+    print(f'{args.first} - {args.second} = {subtract(args.first, args.second)}')
